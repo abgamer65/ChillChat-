ab07607e65a963b61bca3f83c3e9c9efad06bb9f
abgamer65 <89385202+abgamer65@users.noreply.github.com>
2024-06-11 09:56:43 +0500

    Update userinfo.js

--- utilityCommands/userinfo.js
+++ utilityCommands/userinfo.js
@@ -13,8 +13,8 @@ module.exports = {
         .setColor('#FFFFFF')
         .setAuthor({
         name: 'User Info!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1140841446228897932/1144684108174348318/giphy_1.gif', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        iconURL: 'https://media.discordapp.net/attachments/1165539147881132142/1166348715850735616/6dbd7d91d3fc153376a97682697bb14e107c5f337a992b0ee0c6456f137bf015.gif?ex=6668e823&is=666796a3&hm=36c0f4bca0045c201025858fb1dac71be849ed8b7d478f8c7dfde344173d33bd&', 
+        url: 'https://discord.gg/RfQ27TH3'
     })
         .setThumbnail(user.avatarURL({ format: 'png', dynamic: true, size: 1024 }))
         .setDescription(`**Username**:  ${user.username}\n**Discriminator**:  ${user.discriminator}\n**ID**:  ${user.id}\n**Joined Discord**:  ${user.createdAt.toUTCString()}`)
